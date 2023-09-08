7a83c8079286b2ebb4a009dde59697b8cb77bb68
unknowntpo <e850506@gmail.com>
2023-09-08 10:04:09 +0800

    (BufferPoolManager) scaffolding methods (FAIL)

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -47,21 +47,66 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
   Page *page = new Page();
   page->page_id_ = *page_id;
   page_table_.emplace(page_id, frame_id);
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, true);
 }
 
 // TODO: impl
 auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType access_type) -> Page * {
-  // return new Page();
+  const std::lock_guard<std::mutex> lock(latch_);
+
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    // not found
+    return nullptr;
+  }
+  frame_id_t id = it->second;
+  return &pages_[id];
 }
 
 // TODO: impl
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unused]] AccessType access_type) -> bool {
   std::cout << "UnpinPage is called" << std::endl;
-  return false;
+  const std::lock_guard<std::mutex> lock(latch_);
+
+  // return false;
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    // not found
+    return false;
+  }
+  frame_id_t id = it->second;
+  Page *page = &pages_[id];
+  if (page->pin_count_ <= 0) return false;
+  page->pin_count_--;
+  return true;
 }
 
 // TODO: impl
-auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool { return false; }
+auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool {
+  std::cout << "FlushPage is called" << std::endl;
+  const std::lock_guard<std::mutex> lock(latch_);
+
+  if (page_id == INVALID_PAGE_ID) return false;
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    // not found
+    return false;
+  }
+  frame_id_t frame_id = it->second;
+  Page *page = &pages_[frame_id];
+  if (page->pin_count_ > 0) return false;
+
+  // Now, page can be flushed.
+  page_table_.erase(page_id);
+
+  // destruct page, re-construct new blank page.
+  pages_[frame_id].~Page();
+  new (&pages_[frame_id]) Page();
+
+  free_list_.push_back(frame_id);
+  return true;
+}
 
 // TODO: impl
 void BufferPoolManager::FlushAllPages() {}

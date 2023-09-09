494aa946dc67706c70a54d0c972834f65d38122f
unknowntpo <e850506@gmail.com>
2023-09-09 16:43:18 +0800

    (BufferPoolManager) fix BufferPoolManager::UnpinPage

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -61,10 +61,12 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
   LOG_INFO("NewPage is called");
   const std::lock_guard<std::mutex> lock(latch_);
 
+  Page *page;
   frame_id_t frame_id;
   if (free_list_.begin() != free_list_.end()) {
     frame_id = *free_list_.begin();
     free_list_.pop_front();
+    page = &pages_[frame_id];
   } else {
     // pick from the replacer
     bool ok = replacer_->Evict(&frame_id);
@@ -72,7 +74,7 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
       LOG_INFO("no page can be created since replacer is full");
       return nullptr;
     }
-    Page *page = &pages_[frame_id];
+    page = &pages_[frame_id];
     if (page->IsDirty()) {
       if (!this->FlushPage(page->GetPageId())) {
         LOG_ERROR("failed to flush the dirty page");
@@ -83,9 +85,9 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
     }
   }
 
-  Page *page = new Page();
   *page_id = this->AllocatePage();
   page->page_id_ = *page_id;
+  page->pin_count_++;
   page_table_.emplace(*page_id, frame_id);
   replacer_->RecordAccess(frame_id);
   replacer_->SetEvictable(frame_id, false);
@@ -93,7 +95,23 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
   return page;
 }
 
-// TODO: impl
+/**
+ * TODO(P1): Add implementation
+ *
+ * @brief Fetch the requested page from the buffer pool. Return nullptr if page_id needs to be fetched from the disk
+ * but all frames are currently in use and not evictable (in another word, pinned).
+ *
+ * First search for page_id in the buffer pool. If not found, pick a replacement frame from either the free list or
+ * the replacer (always find from the free list first), read the page from disk by calling disk_manager_->ReadPage(),
+ * and replace the old page in the frame. Similar to NewPage(), if the old page is dirty, you need to write it back
+ * to disk and update the metadata of the new page
+ *
+ * In addition, remember to disable eviction and record the access history of the frame like you did for NewPage().
+ *
+ * @param page_id id of page to be fetched
+ * @param access_type type of access to the page, only needed for leaderboard tests.
+ * @return nullptr if page_id cannot be fetched, otherwise pointer to the requested page
+ */
 auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType access_type) -> Page * {
   const std::lock_guard<std::mutex> lock(latch_);
 
@@ -106,7 +124,20 @@ auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType
   return &pages_[id];
 }
 
-// TODO: impl
+/**
+ * TODO(P1): Add implementation
+ *
+ * @brief Unpin the target page from the buffer pool. If page_id is not in the buffer pool or its pin count is already
+ * 0, return false.
+ *
+ * Decrement the pin count of a page. If the pin count reaches 0, the frame should be evictable by the replacer.
+ * Also, set the dirty flag on the page to indicate if the page was modified.
+ *
+ * @param page_id id of page to be unpinned
+ * @param is_dirty true if the page should be marked as dirty, false otherwise
+ * @param access_type type of access to the page, only needed for leaderboard tests.
+ * @return false if the page is not in the page table or its pin count is <= 0 before this call, true otherwise
+ */
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unused]] AccessType access_type) -> bool {
   std::cout << "UnpinPage is called" << std::endl;
   const std::lock_guard<std::mutex> lock(latch_);
@@ -117,10 +148,14 @@ auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unus
     // not found
     return false;
   }
+  // found target page, now unpin it.
   frame_id_t id = it->second;
   Page *page = &pages_[id];
   if (page->pin_count_ <= 0) return false;
   page->pin_count_--;
+  if (page->pin_count_ == 0) {
+    replacer_->SetEvictable(id, true);
+  }
   return true;
 }
 
